98344095734c6a94c246239430f23412b9f2c033
chikiryava <98350681+chikiryava@users.noreply.github.com>
2023-06-20 14:34:55 +0500

    Add files via upload

---
+++ App2/App2/Start.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.CommunityToolkit.Extensions;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Start :ContentPage
+    {
+        static string login;
+        static string password;
+        public Start ()
+        {
+            InitializeComponent();
+        }
+        private bool CheckFields(string _login,string _password)
+        {
+            if (_login.Length <=2)
+            {
+                this.DisplayToastAsync("Длина логина должна быть больше 2 символов");
+                return false;
+            }
+            if (_password.Length <= 4)
+            {
+                this.DisplayToastAsync("Длина пароля должна быть больше 4 символов");
+                return false;
+            }
+            login = _login;
+            password = _password;
+            return true;
+        }
+
+        private void LogButton_Clicked (object sender, EventArgs e)
+        {
+            if(CheckFields(Login.Text, Password.Text))
+            {
+                Navigation.PushAsync(new MainPage(login, password));
+            }
+        }
+    }
+}
